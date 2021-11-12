bc37e49ea871239c558e42b627b46affbf8e040b
Taylor Fausak <taylor@fausak.me>
2021-11-12 13:30:03 +0000

    Stub out library

---
+++ source/library/Monadoc/Main.hs
@@ -0,0 +1,12 @@
+module Monadoc.Main where
+
+import qualified System.Environment as Environment
+
+defaultMain :: IO ()
+defaultMain = do
+    name <- Environment.getProgName
+    arguments <- Environment.getArgs
+    mainWith name arguments
+
+mainWith :: String -> [String] -> IO ()
+mainWith _ _ = pure ()
